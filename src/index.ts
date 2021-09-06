96062f41ef2aeabb6e594e721b8120b247674255
dzucconi <mail@damonzucconi.com>
2021-09-06 14:01:26 -0400

    Adjusts zoom

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ const init = async () => {
   if (window.location.search.includes("zoom")) {
     const zoom = async () => {
       panTo(map, randomCity());
-      await _zoom(map, range(4, 20, 1), 250, 250);
+      await _zoom(map, range(4, 22, 1), 250, 250);
       await zoom();
     };
 
